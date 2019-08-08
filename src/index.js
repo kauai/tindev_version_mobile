d3167ed5e20e61471c5bac9ee6d6bfb180ccb7c1
Thiago kauai <html5ephp@gmail.com>
2019-08-08 12:01:40 -0300

    Criando rotas

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React, {Fragment} from 'react';
+import {View, Text,StyleSheet } from 'react-native';
+import Login from './pages/Login'
+import Routes from './routes'
+
+function App(){
+  return (
+        <Routes/>
+   );
+}
+
+export default App;
